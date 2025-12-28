5315f9bf26d1e7d7d86119e163c654a68cb1bae9
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-28 13:14:36 -0800

    use single script to set env var

---
+++ scripts/hooks/set_environment_variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO actually update
+# set environment variable for ensure version updated
+export PROJECT_VERSION_FILE='proj.json'
+export PROJECT_VERSION_LINE_PATTERN='"version" : "[0-9.]+"'

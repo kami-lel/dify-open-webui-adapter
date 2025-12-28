c862b0781403fff673203b7e134677a6ffeae119
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-28 14:36:34 -0800

    update capture group

--- scripts/hooks/set_environment_variables.sh
+++ scripts/hooks/set_environment_variables.sh
@@ -2,4 +2,4 @@
 
 # set environment variable for ensure version updated
 export PROJECT_VERSION_FILE='dify_open_webui_adapter.py'
-export PROJECT_VERSION_LINE_PATTERN='^__version__ *= *"[0-9.]+"$'
+export PROJECT_VERSION_LINE_PATTERN='^__version__ *= *"([0-9.]+)"$'

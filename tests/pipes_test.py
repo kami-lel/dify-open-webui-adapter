3f7a13a1b9d07d1faa4a3c85316ae22acd1ab1c1
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-03 15:37:24 -0800

    finish pipes tests

--- tests/pipes_test.py
+++ tests/pipes_test.py
@@ -7,4 +7,47 @@ Unit Tests (using pytest) for:
 - BaseContainer.get_model_id_and_name()
 """
 
-# TODO
+from dify_open_webui_adapter import Pipe
+from tests import EXAMPLE_CHATFLOW_CONFIG, EXAMPLE_CONFIGS
+
+
+class TestPipes:
+
+    def test1(_):
+        configs = [EXAMPLE_CHATFLOW_CONFIG]
+        pipe = Pipe(app_model_configs=configs)
+        opt = pipe.pipes()
+
+        print(opt)
+
+        assert isinstance(opt, list)
+        assert len(opt) == 1
+
+        assert opt[0] == {
+            "id": "example-chatflow-model",
+            "name": "Example Chatflow Model/App",
+        }
+
+    def test2(_):
+        pipe = Pipe(app_model_configs=EXAMPLE_CONFIGS)
+        opt = pipe.pipes()
+
+        print(opt)
+
+        assert isinstance(opt, list)
+        assert len(opt) == 3
+
+        assert opt[0] == {
+            "id": "example-workflow-model",
+            "name": "example-workflow-model",
+        }
+
+        assert opt[1] == {
+            "id": "example-chatflow-model",
+            "name": "Example Chatflow Model/App",
+        }
+
+        assert opt[2] == {
+            "id": "example-chatflow-model-2",
+            "name": "Aux Example Chatflow Model/App",
+        }

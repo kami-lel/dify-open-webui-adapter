5fd4815d80ea403e9a71cb00a784f8e447279084
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-01-22 01:14:46 -0800

    remove outdated usage instructions from module docstring
    #`dify_open_webui_adapter.py`

--- dify_open_webui_adapter.py
+++ dify_open_webui_adapter.py
@@ -5,32 +5,6 @@ Integrate Open WebUI and Dify by exposing a Dify App
 Supported Open WebUI Version:   v0.7.1
 Supported Dify Version:         1.11.2
 
-User must configure these 2 constant in Python script before use:
-
-``DIFY_BACKEND_API_BASE_URL``: base URL to access Dify Backend Service API
-
-``APP_MODEL_CONFIGS``: a ``list`` of model/app config (each as ``dict``)
-
-example for a single model/app::
-
-    {
-        "key": "...",             # Backend Service API secret key of Dify App
-        "model_id": "model_id1",  # model id as used in Open WebUI
-        "name": "First Model",    # model Name as appeared in Open WebUI
-    }
-
-example for ``APP_MODEL_CONFIGS``::
-
-    APP_MODEL_CONFIGS = [
-        {
-            "key": "...",
-            "model_id": "model_id1",
-        },
-        {
-            ~  # config for 2nd app/model
-        },
-    ]
-
 Q.v. ``https://github.com/kami-lel/dify-open-webui-adapter``
 """
 

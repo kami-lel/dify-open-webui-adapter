e0966b12463259ed0430a88813431549ce1813d7
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-03-03 00:01:40 -0800

    add pytest test scaffold, fixtures for BaseDifyApp, OWUModel in test suite
    ^`app-base-init_test.py` add initial unit test scaffold for BaseDifyApp.__init__
    ^`conftest.py` add pytest fixture for OWUModel with config and base URL

---
+++ tests/app/app-base-init_test.py
@@ -0,0 +1,13 @@
+"""
+app-base-init_test.py
+
+Unit Tests (using pytest) for:
+
+BaseDifyApp.__init__()
+"""
+
+
+class TestInit:
+
+    def test1(_):
+        pass  # TODO
---
+++ tests/app/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from dify_open_webui_adapter import OWUModel
+
+
+@pytest.fixture
+def model1(base_url, workflow_config1):
+    return OWUModel(
+        base_url,
+        workflow_config1,
+        disable_get_app_type_and_name=True,
+    )

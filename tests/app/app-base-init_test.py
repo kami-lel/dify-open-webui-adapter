e7a858c5b1553bb68a5d99057ae2c71a55805aae
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-03-03 21:14:40 -0800

    rename fixture local_workflow_app to local_wf_app and add tests for model_id and http_header
    |`app-base-init_test.py`

--- tests/app/app-base-init_test.py
+++ tests/app/app-base-init_test.py
@@ -14,18 +14,31 @@ import pytest
 
 # pytest fixture  ##############################################################
 @pytest.fixture
-def local_workflow_app(workflow_model1):
+def local_wf_app(workflow_model1):
     return workflow_model1.app
 
 
 # tests  #######################################################################
 class Test1:
 
-    def test_init(_, local_model_app, workflow_model1):
-        app = local_model_app
+    def test_init(_, local_wf_app, workflow_model1):
+        app = local_wf_app
         model = workflow_model1
         assert app.model is model
 
-    def test_base_url(_, local_workflow_app, base_url):
-        app = local_workflow_app
+    def test_base_url(_, local_wf_app, base_url):
+        app = local_wf_app
         assert app.base_url == base_url
+
+    def test_model_id(_, local_wf_app):
+        app = local_wf_app
+        model_id = "example-workflow-model"
+        assert app.model_id == model_id
+
+    def test_http_header1(_, local_wf_app, workflow_model1):
+        app = local_wf_app
+        assert app.http_header(False) == workflow_model1.http_header(False)
+
+    def test_http_header2(_, local_wf_app, workflow_model1):
+        app = local_wf_app
+        assert app.http_header(True) == workflow_model1.http_header(True)

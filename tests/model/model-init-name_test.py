168729490049b9fd3dcd81f01966af454789c767
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-03-08 12:43:16 -0700

    add pytest unit tests for OWUModel.name with fixtures
    /`model-init-name_test.py`

--- tests/model/model-init-name_test.py
+++ tests/model/model-init-name_test.py
@@ -4,6 +4,58 @@ model-init-name_test.py
 Unit Tests (using pytest) for: OWUModel.name
 """
 
+from dify_open_webui_adapter import (
+    OWUModel,
+    DifyAppType,
+)
+
+import pytest
+
+
+# pytest fixtures  #############################################################
+@pytest.fixture(scope="session")
+def wf_model_provided1(base_url, workflow_config1):
+    config = workflow_config1.copy()
+    config["name"] = "My Workflow Name"
+    return OWUModel(
+        base_url,
+        config,
+        skip_get_app_type_and_name=True,
+        app_type_override=DifyAppType.WORKFLOW,
+    )
+
+
+# provided
+class TestProvided:  ###########################################################
+
+    def test1(_, wf_model_provided1):
+        opt = wf_model_provided1.name
+
+        print(opt)
+        assert isinstance(opt, str)
+        assert opt == "My Workflow Name"
+
+    def test2(_, cf_model_skip1):
+        opt = cf_model_skip1.name
+
+        print(opt)
+        assert isinstance(opt, str)
+        assert opt == "Example Chatflow Model/App"
+
+    def test3(_, cf_model_skip2):
+        opt = cf_model_skip2.name
+
+        print(opt)
+        assert isinstance(opt, str)
+        assert opt == "Aux Example Chatflow Model/App"
+
+
+# from response  ###############################################################
+
+
+# model id  ####################################################################
+
+
 # HACK
 # class TestName:
 

39150b747f98023aaba6ad64858dda341dfbf7b3
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-03-10 03:52:21 -0700

    add pytest-asyncio support; convert test to async; assert error message text
    /`requirements.txt` add pytest-asyncio to requirements for async test support
    ~`pipe-pipe_test.py` make test_no_model async and await pipe call to fix test

--- tests/pipe/pipe-pipe_test.py
+++ tests/pipe/pipe-pipe_test.py
@@ -12,17 +12,15 @@ import pytest
 # pytest  ######################################################################
 class TestErr:  # ==============================================================
 
-    def test_no_model(_, pipe1, pipe_args_no_stream1):
+    @pytest.mark.asyncio
+    async def test_no_model(_, pipe1, pipe_args_no_stream1):
         body, user, metadata = pipe_args_no_stream1
         del body["model"]
 
         with pytest.raises(IndexError) as exec_info:
-            pipe1.pipe(body, user, metadata)
+            await pipe1.pipe(body, user, metadata)
 
         opt = exec_info.value.args[0]
 
         print(opt)
-        assert opt == ""
-
-        pipe1.pipe()
-        pass
+        assert opt == "missing entry 'model' in body"

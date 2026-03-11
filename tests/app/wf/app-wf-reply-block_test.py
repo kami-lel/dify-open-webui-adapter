1f4806aaed51b10e14c481db65df11bbc255501a
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-03-10 21:04:26 -0700

    rename test_no_stream to test_dft and add todo for alt input field
    ~`app-wf-reply-block_test.py`

--- tests/app/wf/app-wf-reply-block_test.py
+++ tests/app/wf/app-wf-reply-block_test.py
@@ -15,7 +15,7 @@ import pytest
 # pytest  ######################################################################
 class TestBlock:
 
-    def test_no_stream(_, app_skip_wf1, patch_reply_no_stream):
+    def test_dft(_, app_skip_wf1, patch_reply_no_stream):
         app = app_skip_wf1
         app.current_user_msg_content = "PRIMARY"
         app.current_enable_stream = False
@@ -31,6 +31,8 @@ class TestBlock:
             assert opt == "DIFY REPLIED MESSAGE"
 
             mock_post.assert_called_once_with(*assert_args, **assert_kwargs)
+
+    # TODO alt input field
 
     # err handling  ============================================================
 

c96995cba6b8f3a9cfe6a2a6d3a175a5aa362e8d
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-19 00:55:18 -0700

    Add unit tests for AppModelConfig.validate_app_model_configs()
    ?`config-validate_test.py`

--- tests/config/config-validate_test.py
+++ tests/config/config-validate_test.py
@@ -1,1 +1,16 @@
-# TODO write unit tests
+"""
+config-validate_test.py
+
+Unit Tests (using pytest) for:
+
+AppModelConfig.validate_app_model_configs()
+"""
+
+from dify_open_webui_adapter import AppModelConfig
+
+
+# Pytest unit tests  ###########################################################
+class TestValidate:
+
+    def test1(_):
+        pass  # TODO write unit tests

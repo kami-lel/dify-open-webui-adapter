e3b9687c70992cad6432ce1974a94d00e3a97b0b
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-21 02:00:04 -0700

    Refactor test to remove unused mock imports and update tests for display_name
    ~`model-init-name_test.py`

--- tests/model/model-init-name_test.py
+++ tests/model/model-init-name_test.py
@@ -6,13 +6,7 @@ Unit Tests (using pytest) for:
 setting .display_name during OWUModel.__init__()
 """
 
-from unittest.mock import patch, Mock
-
-
-from dify_open_webui_adapter import (
-    OWUModel,
-    DifyAppType,
-)
+from dify_open_webui_adapter import OWUModel
 
 # Pytest unit tests  ###########################################################
 
@@ -31,4 +25,26 @@ class TestName:
         assert isinstance(opt, str)
         assert opt == app_given_name_wf1
 
-    # FIXME update
+    def test2(_, config_cf2, app_direct_cf1, app_response_name_cf1):
+        config = config_cf2
+        app = app_direct_cf1
+
+        model = OWUModel(config, app)
+
+        opt = model.display_name
+        print(opt)
+
+        assert isinstance(opt, str)
+        assert opt == app_response_name_cf1
+
+    def test3(_, config_cf2, app_direct_cf2):
+        config = config_cf2
+        app = app_direct_cf2
+
+        model = OWUModel(config, app)
+
+        opt = model.display_name
+        print(opt)
+
+        assert isinstance(opt, str)
+        assert opt == "example-chatflow-model-2"

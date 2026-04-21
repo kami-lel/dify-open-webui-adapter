dcec40314710918d83e20d34b66dd24b3237a71a
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-20 21:10:09 -0700

    Add type validation tests for 'key' and 'model_id' fields in config-init_test.py
    ~`config-init_test.py`

--- tests/config/config-init_test.py
+++ tests/config/config-init_test.py
@@ -63,6 +63,19 @@ class TestKey:  # ==============================================================
         assert opt[0]["loc"] == ("key",)
         assert opt[0]["msg"] == "Field required"
 
+    def test_type(_, config_wf1):
+        raw = copy.copy(config_wf1)
+        raw["key"] = 123
+
+        with pytest.raises(ValidationError) as exec_info:
+            AppModelConfig(**raw)
+
+        opt = exec_info.value.errors()
+        print(opt)
+        assert len(opt) == 1
+        assert opt[0]["loc"] == ("key",)
+        assert opt[0]["msg"] == "Input should be a valid string"
+
     def test_empty(_, config_wf1):
         raw = copy.copy(config_wf1)
         raw["key"] = ""
@@ -91,6 +104,19 @@ class TestId:  # ===============================================================
         assert len(opt) == 1
         assert opt[0]["loc"] == ("model_id",)
         assert opt[0]["msg"] == "Field required"
+
+    def test_type(_, config_wf1):
+        raw = copy.copy(config_wf1)
+        raw["model_id"] = 123
+
+        with pytest.raises(ValidationError) as exec_info:
+            AppModelConfig(**raw)
+
+        opt = exec_info.value.errors()
+        print(opt)
+        assert len(opt) == 1
+        assert opt[0]["loc"] == ("model_id",)
+        assert opt[0]["msg"] == "Input should be a valid string"
 
     def test_empty(_, config_wf1):
         raw = copy.copy(config_wf1)

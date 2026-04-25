c26438116127dd198da3bca19b7b5450383052b5
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-24 18:21:04 -0700

    add model/app init tests for workflow and chatflow mappings
    ~`pipe-init-apps_test.py` Refactor tests: add app type, config, model, name checks for WorkflowApp, ChatflowApp
    =`pipe-init-models_test.py` Add tests for models configuration and app type checks

--- tests/pipe/pipe-init-apps_test.py
+++ tests/pipe/pipe-init-apps_test.py
@@ -6,6 +6,8 @@ Unit Tests (using pytest) for:
 Pipe.__init__() populating .apps
 """
 
+from dify_open_webui_adapter import ChatflowApp, WorkflowApp
+
 # Pytest unit tests  ###########################################################
 
 
@@ -33,4 +35,79 @@ class TestKeys:  # =============================================================
         assert key in opt
 
 
-# TODO
+class TestWf1:  # ==============================================================
+
+    def test_type(_, pipe_obj, model_id_wf1):
+        app = pipe_obj.apps[model_id_wf1]
+        print(app)
+        assert isinstance(app, WorkflowApp)
+
+    def test_config(_, pipe_obj, model_id_wf1, config_wf1):
+        app = pipe_obj.apps[model_id_wf1]
+
+        opt = app.config
+        print(opt)
+        assert opt == config_wf1
+
+    def test_model(_, pipe_obj, model_id_wf1):
+        app = pipe_obj.apps[model_id_wf1]
+        model = pipe_obj.models[model_id_wf1]
+        assert app.model is model
+
+    def test_name(_, pipe_obj, model_id_wf1, app_response_name_wf1):
+        app = pipe_obj.apps[model_id_wf1]
+        opt = app.response_name
+        print(opt)
+        assert opt == app_response_name_wf1
+
+
+class TestCf1:  # ==============================================================
+
+    def test_type(_, pipe_obj, model_id_cf1):
+        app = pipe_obj.apps[model_id_cf1]
+        assert isinstance(app, ChatflowApp)
+
+    def test_config(_, pipe_obj, model_id_cf1, config_cf1):
+        app = pipe_obj.apps[model_id_cf1]
+
+        opt = app.config
+        print(opt)
+        assert opt == config_cf1
+
+    def test_model(_, pipe_obj, model_id_cf1):
+        app = pipe_obj.apps[model_id_cf1]
+        model = pipe_obj.models[model_id_cf1]
+        assert app.model is model
+
+    def test_name(_, pipe_obj, model_id_cf1, app_response_name_cf1):
+        app = pipe_obj.apps[model_id_cf1]
+
+        opt = app.response_name
+        print(opt)
+        assert opt == app_response_name_cf1
+
+
+class TestCf2:  # ==============================================================
+
+    def test_type(_, pipe_obj, model_id_cf2):
+        app = pipe_obj.apps[model_id_cf2]
+        assert isinstance(app, ChatflowApp)
+
+    def test_config(_, pipe_obj, model_id_cf2, config_cf2):
+        app = pipe_obj.apps[model_id_cf2]
+
+        opt = app.config
+        print(opt)
+        assert opt == config_cf2
+
+    def test_model(_, pipe_obj, model_id_cf2):
+        app = pipe_obj.apps[model_id_cf2]
+        model = pipe_obj.models[model_id_cf2]
+        assert app.model is model
+
+    def test_name(_, pipe_obj, model_id_cf2):
+        app = pipe_obj.apps[model_id_cf2]
+
+        opt = app.response_name
+        print(opt)
+        assert opt is None
--- tests/pipe/pipe-init-models_test.py
+++ tests/pipe/pipe-init-models_test.py
@@ -9,6 +9,9 @@ Pipe.__init__() populating .models
 # Pytest unit tests  ###########################################################
 
 
+from dify_open_webui_adapter import AppModelConfig, ChatflowApp, WorkflowApp
+
+
 class TestKeys:  # =============================================================
 
     def test_wf1(_, pipe_obj, model_id_wf1):
@@ -33,4 +36,64 @@ class TestKeys:  # =============================================================
         assert key in opt
 
 
-# TODO
+class TestWf1:  # ==============================================================
+
+    def test_config(_, pipe_obj, model_id_wf1, config_wf1):
+        model = pipe_obj.models[model_id_wf1]
+        config = config_wf1
+
+        assert isinstance(model.config, AppModelConfig)
+        assert model.config == config
+
+    def test_app(_, pipe_obj, model_id_wf1):
+        model = pipe_obj.models[model_id_wf1]
+        app = pipe_obj.apps[model_id_wf1]
+
+        assert isinstance(model.app, WorkflowApp)
+        assert model.app is app
+
+    def test_call(_, pipe_obj, model_id_wf1):
+        model = pipe_obj.models[model_id_wf1]
+        assert model.call is None
+
+
+class TestCf1:  # ==============================================================
+
+    def test_config(_, pipe_obj, model_id_cf1, config_cf1):
+        model = pipe_obj.models[model_id_cf1]
+        config = config_cf1
+
+        assert isinstance(model.config, AppModelConfig)
+        assert model.config == config
+
+    def test_app(_, pipe_obj, model_id_cf1):
+        model = pipe_obj.models[model_id_cf1]
+        app = pipe_obj.apps[model_id_cf1]
+
+        assert isinstance(model.app, ChatflowApp)
+        assert model.app is app
+
+    def test_call(_, pipe_obj, model_id_cf1):
+        model = pipe_obj.models[model_id_cf1]
+        assert model.call is None
+
+
+class TestCf2:  # ==============================================================
+
+    def test_config(_, pipe_obj, model_id_cf2, config_cf2):
+        model = pipe_obj.models[model_id_cf2]
+        config = config_cf2
+
+        assert isinstance(model.config, AppModelConfig)
+        assert model.config == config
+
+    def test_app(_, pipe_obj, model_id_cf2):
+        model = pipe_obj.models[model_id_cf2]
+        app = pipe_obj.apps[model_id_cf2]
+
+        assert isinstance(model.app, ChatflowApp)
+        assert model.app is app
+
+    def test_call(_, pipe_obj, model_id_cf2):
+        model = pipe_obj.models[model_id_cf2]
+        assert model.call is None

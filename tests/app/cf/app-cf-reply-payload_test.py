243ddfddd3bbad209347f233cb80e826a0f1f5ea
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-25 15:01:29 -0700

    rename reply-payload tests for _chat_payload()
    ~`app-cf-reply-payload_test.py` refactor test to rename _create_reply_payload to _chat_payload
    ~`app-wf-reply-payload_test.py` Rename test function from _create_reply_payload to _chat_payload

--- tests/app/cf/app-cf-reply-payload_test.py
+++ tests/app/cf/app-cf-reply-payload_test.py
@@ -3,7 +3,7 @@ app-cf-reply-payload_test.py
 
 Unit Tests (using pytest) for:
 
-ChatflowApp._create_reply_payload()
+ChatflowApp._chat_payload()
 """
 
 
--- tests/app/wf/app-wf-reply-payload_test.py
+++ tests/app/wf/app-wf-reply-payload_test.py
@@ -3,7 +3,7 @@ app-wf-reply-payload_test.py
 
 Unit Tests (using pytest) for:
 
-WorkflowApp._create_reply_payload()
+WorkflowApp._chat_payload()
 """
 
 

ff7dd9d68756758e7a7f11102cac62c1d0bc1399
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-04-25 14:41:05 -0700

    refactor tests, remove fixtures, streamline PipeCall message tests
    ~`call-msg_test.py`

--- tests/call/call-msg_test.py
+++ tests/call/call-msg_test.py
@@ -8,55 +8,50 @@ PipeCall.message
 
 import pytest
 
-# Pytest fixtures  #############################################################
-
-
-@pytest.fixture(scope="class")
-def local_call(pipe_call):
-    call = pipe_call.model_copy()
-    call.body.messages = [
-        {"role": "user", "content": "FIRST USER MESSAGE"},
-        {"role": "assistant", "content": "FIRST BOT REPLY"},
-        {"role": "user", "content": "SECOND USER MESSAGE"},
-        {"role": "assistant", "content": "SECOND BOT REPLY"},
-        {"role": "user", "content": "THIRD USER MESSAGE"},
-    ]
-    return call
-
+from dify_open_webui_adapter import PipeCall
 
 # Pytest unit tests  ###########################################################
 
 
 class TestGet:
 
-    # FIXME
+    def test1(_, pipe_call_args):
+        args = pipe_call_args
 
-    def test1(_, model_wf_skip1, pipe_body1):
-        model = model_wf_skip1
-        body = pipe_body1
+        call = PipeCall(**args)
 
-        opt = model._get_last_user_msg_content(body)
-        print(opt)
-        assert opt == "FIRST USER MESSAGE"
+        opt = call.message
+        assert opt == "Hello Dify"
 
-    def test2(_, model_cf_skip1, pipe_body2):
-        model = model_cf_skip1
-        body = pipe_body2
+    def test2(_, pipe_call_args):
+        args = pipe_call_args
+        args["body"]["messages"] = [
+            {"role": "user", "content": "FIRST USER MESSAGE"},
+            {"role": "assistant", "content": "FIRST BOT REPLY"},
+            {"role": "user", "content": "SECOND USER MESSAGE"},
+            {"role": "assistant", "content": "SECOND BOT REPLY"},
+            {"role": "user", "content": "THIRD USER MESSAGE"},
+        ]
 
-        opt = model._get_last_user_msg_content(body)
-        print(opt)
+        call = PipeCall(**args)
+
+        opt = call.message
         assert opt == "THIRD USER MESSAGE"
 
     # err handling  ============================================================
 
-    def test_no_user1(_, model_wf_skip1, pipe_body1):
-        model = model_wf_skip1
-        body = pipe_body1
-        body["messages"] = []
+    def test_no_user1(_, pipe_call_args):
+        args = pipe_call_args
+        args["body"]["messages"] = []
 
         with pytest.raises(ValueError) as exec_info:
-            model._get_last_user_msg_content(body)
-        opt = exec_info.value.args[0]
+            PipeCall(**args)
 
+        opt = exec_info.value.errors()
         print(opt)
-        assert opt == "missing user message in body"
+        assert len(opt) == 1
+        assert opt[0]["loc"] == ("body", "messages")
+        assert (
+            opt[0]["msg"]
+            == "List should have at least 1 item after validation, not 0"
+        )
